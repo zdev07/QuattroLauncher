77a31ce60f4b5d637c4cd57b4c5d088d18babd33
DomainPixel <funpasscloud518@gmail.com>
2026-01-13 23:08:24 +0530

    Update ProgressLayout.java

--- app_pojavlauncher/src/main/java/com/kdt/mcgui/ProgressLayout.java
+++ app_pojavlauncher/src/main/java/com/kdt/mcgui/ProgressLayout.java
@@ -1,6 +1,5 @@
 package com.kdt.mcgui;
 
-
 import android.content.Context;
 import android.util.AttributeSet;
 import android.util.Log;
@@ -12,25 +11,19 @@ import android.widget.TextView;
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.annotation.StringRes;
-import androidx.collection.ArrayMap;
 import androidx.constraintlayout.widget.ConstraintLayout;
 
 import net.kdt.pojavlaunch.R;
-import net.kdt.pojavlaunch.extra.ExtraCore;
 import net.kdt.pojavlaunch.progresskeeper.ProgressKeeper;
 import net.kdt.pojavlaunch.progresskeeper.ProgressListener;
 import net.kdt.pojavlaunch.progresskeeper.TaskCountListener;
-import net.kdt.pojavlaunch.services.ProgressService;
 
 import java.util.ArrayList;
 
-
-/** Class staring at specific values and automatically show something if the progress is present
- * Since progress is posted in a specific way, The packing/unpacking is handheld by the class
- *
- * This class relies on ExtraCore for its behavior.
+/** * Quattro Progress Layout
+ * Monitors background tasks and displays them to the user.
  */
-public class ProgressLayout extends ConstraintLayout implements View.OnClickListener, TaskCountListener{
+public class ProgressLayout extends ConstraintLayout implements View.OnClickListener, TaskCountListener {
     public static final String UNPACK_RUNTIME = "unpack_runtime";
     public static final String DOWNLOAD_MINECRAFT = "download_minecraft";
     public static final String DOWNLOAD_VERSION_LIST = "download_verlist";
@@ -39,31 +32,27 @@ public class ProgressLayout extends ConstraintLayout implements View.OnClickList
     public static final String EXTRACT_COMPONENTS = "extract_components";
     public static final String EXTRACT_SINGLE_FILES = "extract_single_files";
 
-    public ProgressLayout(@NonNull Context context) {
-        super(context);
-        init();
-    }
-    public ProgressLayout(@NonNull Context context, @Nullable AttributeSet attrs) {
-        super(context, attrs);
-        init();
-    }
-    public ProgressLayout(@NonNull Context context, @Nullable AttributeSet attrs, int defStyleAttr) {
-        super(context, attrs, defStyleAttr);
-        init();
-    }
-    public ProgressLayout(@NonNull Context context, @Nullable AttributeSet attrs, int defStyleAttr, int defStyleRes) {
-        super(context, attrs, defStyleAttr, defStyleRes);
-        init();
-    }
-
     private final ArrayList<LayoutProgressListener> mMap = new ArrayList<>();
     private LinearLayout mLinearLayout;
     private TextView mTaskNumberDisplayer;
     private ImageView mFlipArrow;
 
+    public ProgressLayout(@NonNull Context context) { super(context); init(); }
+    public ProgressLayout(@NonNull Context context, @Nullable AttributeSet attrs) { super(context, attrs); init(); }
+    public ProgressLayout(@NonNull Context context, @Nullable AttributeSet attrs, int defStyleAttr) { super(context, attrs, defStyleAttr); init(); }
 
+    private void init() {
+        inflate(getContext(), R.layout.view_progress, this);
+        mLinearLayout = findViewById(R.id.progress_linear_layout);
+        mTaskNumberDisplayer = findViewById(R.id.progress_textview);
+        mFlipArrow = findViewById(R.id.progress_flip_arrow);
+        
+        // Custom Quattro Theme Color
+        setBackgroundColor(getResources().getColor(R.color.background_bottom_bar));
+        setOnClickListener(this);
+    }
 
-    public void observe(String progressKey){
+    public void observe(String progressKey) {
         mMap.add(new LayoutProgressListener(progressKey));
     }
 
@@ -73,55 +62,35 @@ public class ProgressLayout extends ConstraintLayout implements View.OnClickList
         }
     }
 
-    public boolean hasProcesses(){
-        return ProgressKeeper.getTaskCount() > 0;
-    }
-
-
-    private void init(){
-        inflate(getContext(), R.layout.view_progress, this);
-        mLinearLayout = findViewById(R.id.progress_linear_layout);
-        mTaskNumberDisplayer = findViewById(R.id.progress_textview);
-        mFlipArrow = findViewById(R.id.progress_flip_arrow);
-        setBackgroundColor(getResources().getColor(R.color.background_bottom_bar));
-        setOnClickListener(this);
-    }
-
-
-    /** Update the progress bar content */
-    public static void setProgress(String progressKey, int progress){
+    public static void setProgress(String progressKey, int progress) {
         ProgressKeeper.submitProgress(progressKey, progress, -1, (Object)null);
     }
 
-    /** Update the text and progress content */
-    public static void setProgress(String progressKey, int progress, @StringRes int resource, Object... message){
+    public static void setProgress(String progressKey, int progress, @StringRes int resource, Object... message) {
         ProgressKeeper.submitProgress(progressKey, progress, resource, message);
     }
 
-    /** Update the text and progress content */
-    public static void setProgress(String progressKey, int progress, String message){
-        setProgress(progressKey,progress, -1, message);
-    }
-
-    /** Update the text and progress content */
-    public static void clearProgress(String progressKey){
-        setProgress(progressKey, -1, -1);
+    public static void clearProgress(String progressKey) {
+        ProgressKeeper.submitProgress(progressKey, -1, -1, (Object)null);
     }
 
     @Override
     public void onClick(View v) {
-        mLinearLayout.setVisibility(mLinearLayout.getVisibility() == GONE ? VISIBLE : GONE);
-        mFlipArrow.setRotation(mLinearLayout.getVisibility() == GONE? 0 : 180);
+        boolean isVisible = mLinearLayout.getVisibility() == VISIBLE;
+        mLinearLayout.setVisibility(isVisible ? GONE : VISIBLE);
+        mFlipArrow.setRotation(isVisible ? 0 : 180);
     }
 
     @Override
     public void onUpdateTaskCount(int tc) {
-        post(()->{
-            if(tc > 0) {
+        post(() -> {
+            if (tc > 0) {
+                // Displays "Quattro: X tasks in progress"
                 mTaskNumberDisplayer.setText(getContext().getString(R.string.progresslayout_tasks_in_progress, tc));
                 setVisibility(VISIBLE);
-            }else
+            } else {
                 setVisibility(GONE);
+            }
         });
     }
 
@@ -129,6 +98,7 @@ public class ProgressLayout extends ConstraintLayout implements View.OnClickList
         final String progressKey;
         final TextProgressBar textView;
         final LinearLayout.LayoutParams params;
+
         public LayoutProgressListener(String progressKey) {
             this.progressKey = progressKey;
             textView = new TextProgressBar(getContext());
@@ -137,29 +107,25 @@ public class ProgressLayout extends ConstraintLayout implements View.OnClickList
             params.bottomMargin = getResources().getDimensionPixelOffset(R.dimen._6sdp);
             ProgressKeeper.addListener(progressKey, this);
         }
+
         @Override
         public void onProgressStarted() {
-            post(()-> {
-                Log.i("ProgressLayout", "onProgressStarted");
-                mLinearLayout.addView(textView, params);
-            });
+            post(() -> mLinearLayout.addView(textView, params));
         }
 
         @Override
         public void onProgressUpdated(int progress, int resid, Object... va) {
-            post(()-> {
+            post(() -> {
                 textView.setProgress(progress);
-                if(resid != -1) textView.setText(getContext().getString(resid, va));
-                else if(va.length > 0 && va[0] != null)textView.setText((String)va[0]);
+                if (resid != -1) textView.setText(getContext().getString(resid, va));
+                else if (va.length > 0 && va[0] != null) textView.setText((String) va[0]);
                 else textView.setText("");
             });
         }
 
         @Override
         public void onProgressEnded() {
-            post(()-> {
-                mLinearLayout.removeView(textView);
-            });
+            post(() -> mLinearLayout.removeView(textView));
         }
     }
 }
