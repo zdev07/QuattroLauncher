5258b34bb3bb0fe0c0a9c69006bb7ff814bfb532
DomainPixel <funpasscloud518@gmail.com>
2026-01-13 23:44:20 +0530

    Update ProgressListener.java

--- app_pojavlauncher/src/main/java/net/kdt/pojavlaunch/authenticator/listener/ProgressListener.java
+++ app_pojavlauncher/src/main/java/net/kdt/pojavlaunch/authenticator/listener/ProgressListener.java
@@ -1,7 +1,18 @@
 package net.kdt.pojavlaunch.authenticator.listener;
 
-/** Called when a step is started, guaranteed to be in the UI Thread */
+import net.kdt.pojavlaunch.value.MinecraftAccount;
+
+/** Called when the Quattro login is complete and the account is received. */
+public interface DoneListener {
+    void onLoginDone(MinecraftAccount account);
+}
+
+/** Called when there is a complete failure during Quattro authentication. */
+public interface ErrorListener {
+    void onLoginError(Throwable errorMessage);
+}
+
+/** Called when a Quattro login step is started. */
 public interface ProgressListener {
-    /** */
     void onLoginProgress(int step);
 }
