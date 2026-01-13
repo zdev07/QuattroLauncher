81fb1786a56a758e468c106a60db34f4a8614174
DomainPixel <funpasscloud518@gmail.com>
2026-01-13 23:09:10 +0530

    Update mcAccountSpinner.java

--- app_pojavlauncher/src/main/java/com/kdt/mcgui/mcAccountSpinner.java
+++ app_pojavlauncher/src/main/java/com/kdt/mcgui/mcAccountSpinner.java
@@ -3,7 +3,6 @@ package com.kdt.mcgui;
 import android.animation.ObjectAnimator;
 import android.annotation.SuppressLint;
 import android.content.Context;
-import android.graphics.Bitmap;
 import android.graphics.Canvas;
 import android.graphics.Color;
 import android.graphics.Paint;
@@ -28,14 +27,12 @@ import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.widget.AppCompatSpinner;
 import androidx.core.content.res.ResourcesCompat;
 
-
 import net.kdt.pojavlaunch.PojavProfile;
 import net.kdt.pojavlaunch.R;
 import net.kdt.pojavlaunch.Tools;
 import net.kdt.pojavlaunch.authenticator.listener.DoneListener;
 import net.kdt.pojavlaunch.authenticator.listener.ErrorListener;
 import net.kdt.pojavlaunch.authenticator.listener.ProgressListener;
-import net.kdt.pojavlaunch.authenticator.microsoft.PresentedException;
 import net.kdt.pojavlaunch.authenticator.microsoft.MicrosoftBackgroundLogin;
 import net.kdt.pojavlaunch.extra.ExtraConstants;
 import net.kdt.pojavlaunch.extra.ExtraCore;
@@ -50,361 +47,97 @@ import java.util.List;
 
 import fr.spse.extended_view.ExtendedTextView;
 
+/**
+ * Quattro Account Selector
+ */
 public class mcAccountSpinner extends AppCompatSpinner implements AdapterView.OnItemSelectedListener {
-    public mcAccountSpinner(@NonNull Context context) {
-        this(context, null);
-    }
+    private final List<String> mAccountList = new ArrayList<>(2);
+    private MinecraftAccount mSelectecAccount = null;
+    private final Paint mLoginBarPaint = new Paint();
+    private float mLoginBarWidth = -1;
+    private int mLoginStep = 0;
+
     public mcAccountSpinner(@NonNull Context context, @Nullable AttributeSet attrs) {
         super(context, attrs);
         init();
     }
 
-    private final List<String> mAccountList = new ArrayList<>(2);
-    private MinecraftAccount mSelectecAccount = null;
-
-    /* Display the head of the current profile, here just to allow bitmap recycling */
-    private BitmapDrawable mHeadDrawable;
-
-    /* Current animator to for the login bar, is swapped when changing step */
-    private ObjectAnimator mLoginBarAnimator;
-    private float mLoginBarWidth = -1;
-
-    /* Paint used to display the bottom bar, to show the login progress. */
-    private final Paint mLoginBarPaint = new Paint();
-
-    /* When a login is performed in the background, we need to know where we are */
-    private final static int MAX_LOGIN_STEP = 5;
-    private int mLoginStep = 0;
-
-    /* Login listeners */
-    private final ProgressListener mProgressListener = step -> {
-        // Animate the login bar, cosmetic purposes only
-        mLoginStep = step;
-        if(mLoginBarAnimator != null){
-            mLoginBarAnimator.cancel();
-            mLoginBarAnimator.setFloatValues( mLoginBarWidth, (getWidth()/MAX_LOGIN_STEP * mLoginStep));
-        }else{
-            mLoginBarAnimator = ObjectAnimator.ofFloat(this, "LoginBarWidth", mLoginBarWidth, (getWidth()/MAX_LOGIN_STEP * mLoginStep));
-        }
-        mLoginBarAnimator.start();
-    };
-
-    private final DoneListener mDoneListener = account -> {
-        Toast.makeText(getContext(), R.string.main_login_done, Toast.LENGTH_SHORT).show();
-
-        // Check if the account being added is not one that is already existing
-        // Like login twice on the same mc account...
-        for(String mcAccountName : mAccountList){
-            if(mcAccountName.equals(account.username)) return;
-        }
-
-        mSelectecAccount = account;
-        invalidate();
-        mAccountList.add(account.username);
-        reloadAccounts(false, mAccountList.size() -1);
-    };
-
-    private final ErrorListener mErrorListener = errorMessage -> {
-        mLoginBarPaint.setColor(Color.RED);
-        Context context = getContext();
-        if(errorMessage instanceof PresentedException) {
-            PresentedException exception = (PresentedException) errorMessage;
-            Throwable cause = exception.getCause();
-            if(cause == null) {
-                Tools.dialog(context, context.getString(R.string.global_error), exception.toString(context));
-            }else {
-                Tools.showError(context, exception.toString(context), exception.getCause());
-            }
-        }else {
-            Tools.showError(getContext(), errorMessage);
-        }
-        invalidate();
-    };
-
-    /* Triggered when we need to do microsoft login */
-    private final ExtraListener<Uri> mMicrosoftLoginListener = (key, value) -> {
-        mLoginBarPaint.setColor(getResources().getColor(R.color.minebutton_color));
-        new MicrosoftBackgroundLogin(false, value.getQueryParameter("code")).performLogin(
-                mProgressListener, mDoneListener, mErrorListener);
-        return false;
-    };
-
-    /* Triggered when we need to perform mojang login */
-    private final ExtraListener<String[]> mMojangLoginListener = (key, value) -> {
-        if(value[1].isEmpty()){ // Test mode
-            MinecraftAccount account = new MinecraftAccount();
-            account.username = value[0];
-            try {
-                account.save();
-            }catch (IOException e){
-                Log.e("McAccountSpinner", "Failed to save the account : " + e);
-            }
-
-            mDoneListener.onLoginDone(account);
-        }
-        return false;
-    };
-
-
-    @SuppressLint("ClickableViewAccessibility")
-    private void init(){
-        // Set visual properties
+    private void init() {
         setBackgroundColor(getResources().getColor(R.color.background_status_bar));
         mLoginBarPaint.setColor(getResources().getColor(R.color.minebutton_color));
         mLoginBarPaint.setStrokeWidth(getResources().getDimensionPixelOffset(R.dimen._2sdp));
 
-        // Set behavior
         reloadAccounts(true, 0);
         setOnItemSelectedListener(this);
 
-        ExtraCore.addExtraListener(ExtraConstants.MOJANG_LOGIN_TODO, mMojangLoginListener);
-        ExtraCore.addExtraListener(ExtraConstants.MICROSOFT_LOGIN_TODO, mMicrosoftLoginListener);
-    }
-
-
-    @Override
-    public final void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
-        if(position == 0){  // Add account button
-            if(mAccountList.size() > 1){
-                ExtraCore.setValue(ExtraConstants.SELECT_AUTH_METHOD, true);
+        ExtraCore.addExtraListener(ExtraConstants.MOJANG_LOGIN_TODO, (key, value) -> {
+            if(value[1].isEmpty()) {
+                MinecraftAccount account = new MinecraftAccount();
+                account.username = value[0];
+                try { account.save(); } catch (IOException e) { Log.e("Quattro", "Save failed", e); }
+                onLoginDone(account);
             }
-            return;
-        }
+            return false;
+        });
 
-        pickAccount(position);
-        if(mSelectecAccount != null)
-            performLogin(mSelectecAccount);
-    }
-
-    @Override
-    public final void onNothingSelected(AdapterView<?> parent) {}
-
-
-    @Override
-    protected void onDraw(Canvas canvas) {
-        if(mLoginBarWidth == -1) mLoginBarWidth = getWidth(); // Initial draw
-
-        float bottom = getHeight() - mLoginBarPaint.getStrokeWidth()/2;
-        canvas.drawLine(0, bottom, mLoginBarWidth, bottom, mLoginBarPaint);
-    }
-
-    public void removeCurrentAccount(){
-        removeAccount(getSelectedItemPosition());
-    }
-
-    private void removeAccount(int position) {
-        if(position == 0) return;
-        File accountFile = new File(Tools.DIR_ACCOUNT_NEW, mAccountList.get(position)+".json");
-        if(accountFile.exists()) accountFile.delete();
-        mAccountList.remove(position);
-
-        reloadAccounts(false, 0);
-    }
-
-    @Keep
-    public void setLoginBarWidth(float value){
-        mLoginBarWidth = value;
-        invalidate(); // Need to redraw each time this is changed
-    }
-
-    /** Allows checking whether we have an online account */
-    public boolean isAccountOnline(){
-        return mSelectecAccount != null && !mSelectecAccount.accessToken.equals("0");
-    }
-
-    public MinecraftAccount getSelectedAccount(){
-        return mSelectecAccount;
-    }
-
-    public int getLoginState(){
-        return mLoginStep;
-    }
-
-    public boolean isLoginDone(){
-        return mLoginStep >= MAX_LOGIN_STEP;
-    }
-
-    @SuppressLint("ClickableViewAccessibility")
-    private void setNoAccountBehavior(){
-        // Set custom behavior when no account are present, to make it act as a button
-        if(mAccountList.size() != 1){
-            // Remove any touch listener
-            setOnTouchListener(null);
-            return;
-        }
-
-        // Make the spinner act like a button, since there is no item to really select
-        setOnTouchListener((v, event) -> {
-            if(event.getAction() != MotionEvent.ACTION_UP) return false;
-            // The activity should intercept this and spawn another fragment
-            ExtraCore.setValue(ExtraConstants.SELECT_AUTH_METHOD, true);
-            return true;
+        ExtraCore.addExtraListener(ExtraConstants.MICROSOFT_LOGIN_TODO, (key, value) -> {
+            new MicrosoftBackgroundLogin(false, value.getQueryParameter("code"))
+                .performLogin(step -> mLoginStep = step, this::onLoginDone, this::onLoginError);
+            return false;
         });
     }
 
-    /**
-     * Reload the spinner, from memory or from scratch. A default account can be selected
-     * @param fromFiles Whether we use files as the source of truth
-     * @param overridePosition Force the spinner to be at this position, if not 0
-     */
-    private void reloadAccounts(boolean fromFiles, int overridePosition){
-        if(fromFiles){
+    private void onLoginDone(MinecraftAccount account) {
+        Toast.makeText(getContext(), "Quattro: Logged in as " + account.username, Toast.LENGTH_SHORT).show();
+        if (!mAccountList.contains(account.username)) {
+            mAccountList.add(account.username);
+        }
+        reloadAccounts(false, mAccountList.indexOf(account.username));
+    }
+
+    private void onLoginError(Object error) {
+        Tools.showError(getContext(), "Quattro Login Error: " + error.toString());
+    }
+
+    private void reloadAccounts(boolean fromFiles, int overridePosition) {
+        if (fromFiles) {
             mAccountList.clear();
-
-            mAccountList.add(getContext().getString(R.string.main_add_account));
+            mAccountList.add("Add Quattro Account");
             File accountFolder = new File(Tools.DIR_ACCOUNT_NEW);
-            if(accountFolder.exists()){
+            if (accountFolder.exists() && accountFolder.list() != null) {
                 for (String fileName : accountFolder.list()) {
-                    mAccountList.add(fileName.substring(0, fileName.length() - 5));
+                    mAccountList.add(fileName.replace(".json", ""));
                 }
             }
         }
-
-        String[] accountArray = mAccountList.toArray(new String[0]);
-        AccountAdapter accountAdapter = new AccountAdapter(getContext(), R.layout.item_minecraft_account, accountArray);
-        accountAdapter.setDropDownViewResource(R.layout.item_minecraft_account);
-        setAdapter(accountAdapter);
-
-        // Pick what's available, might just be the the add account "button"
-        pickAccount(overridePosition == 0 ? -1 : overridePosition);
-        if(mSelectecAccount != null)
-            performLogin(mSelectecAccount);
-
-        // Remove or add the behavior if needed
-        setNoAccountBehavior();
-
+        
+        ArrayAdapter<String> adapter = new AccountAdapter(getContext(), R.layout.item_minecraft_account, mAccountList.toArray(new String[0]));
+        setAdapter(adapter);
+        setSelection(Math.max(0, overridePosition));
     }
 
-    private void performLogin(MinecraftAccount minecraftAccount){
-        if(minecraftAccount.isLocal()) return;
-
-        mLoginBarPaint.setColor(getResources().getColor(R.color.minebutton_color));
-        if(minecraftAccount.isMicrosoft){
-            if(System.currentTimeMillis() > minecraftAccount.expiresAt){
-                // Perform login only if needed
-                new MicrosoftBackgroundLogin(true, minecraftAccount.msaRefreshToken)
-                        .performLogin(mProgressListener, mDoneListener, mErrorListener);
-            }
-            return;
+    @Override
+    public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+        if (position == 0) {
+            ExtraCore.setValue(ExtraConstants.SELECT_AUTH_METHOD, true);
+        } else {
+            mSelectecAccount = PojavProfile.getCurrentProfileContent(getContext(), mAccountList.get(position));
         }
     }
 
-    /** Pick the selected account, the one in settings if 0 is passed */
-    private void pickAccount(int position){
-        MinecraftAccount selectedAccount;
-        if(position != -1){
-            PojavProfile.setCurrentProfile(getContext(), mAccountList.get(position));
-            selectedAccount = PojavProfile.getCurrentProfileContent(getContext(), mAccountList.get(position));
-
-
-            // WORKAROUND
-            // Account file corrupted due to previous versions having improper encoding
-            if (selectedAccount == null){
-                removeCurrentAccount();
-                pickAccount(-1);
-                setSelection(0);
-                return;
-            }
-            setSelection(position);
-        }else {
-            // Get the current profile, or the first available profile if the wanted one is unavailable
-            selectedAccount = PojavProfile.getCurrentProfileContent(getContext(), null);
-            int spinnerPosition = selectedAccount == null
-                    ? mAccountList.size() <= 1 ? 0 : 1
-                    : mAccountList.indexOf(selectedAccount.username);
-            setSelection(spinnerPosition, false);
-        }
-
-        mSelectecAccount = selectedAccount;
-        setImageFromSelectedAccount();
-    }
-
-    @Deprecated()
-    /* Legacy behavior, update the head image manually for the selected account */
-    private void setImageFromSelectedAccount(){
-        BitmapDrawable oldBitmapDrawable = mHeadDrawable;
-
-        if(mSelectecAccount != null){
-            View layout = getSelectedView();
-            if(layout != null){
-                ExtendedTextView view = layout.findViewById(R.id.account_item);
-                Bitmap bitmap = mSelectecAccount.getSkinFace();
-                if(bitmap != null) {
-                    mHeadDrawable = new BitmapDrawable(getResources(), bitmap);
-                    view.setCompoundDrawables(mHeadDrawable, null, null, null);
-                }else{
-                    view.setCompoundDrawables(null, null, null, null);
-                }
-                view.postProcessDrawables();
-            }
-        }
-
-        if(oldBitmapDrawable != null){
-            oldBitmapDrawable.getBitmap().recycle();
-        }
-    }
+    @Override
+    public void onNothingSelected(AdapterView<?> parent) {}
 
     private class AccountAdapter extends ArrayAdapter<String> {
+        public AccountAdapter(Context ctx, int res, String[] items) { super(ctx, res, items); }
 
-        private final HashMap<String, Drawable> mImageCache = new HashMap<>();
-        public AccountAdapter(@NonNull Context context, int resource, @NonNull String[] objects) {
-            super(context, resource, objects);
-        }
-
-        @Override
-        public View getDropDownView(int position, @Nullable View convertView, @NonNull ViewGroup parent) {
-            if(convertView == null){
-                convertView = LayoutInflater.from(parent.getContext()).inflate(R.layout.item_minecraft_account, parent, false);
-            }
-
-            ExtendedTextView textview = convertView.findViewById(R.id.account_item);
-            ImageView deleteButton = convertView.findViewById(R.id.delete_account_button);
-            textview.setText(super.getItem(position));
-
-            // Handle the "Add account section"
-            if(position == 0) {
-                textview.setCompoundDrawables(ResourcesCompat.getDrawable(parent.getResources(), R.drawable.ic_add, null), null, null, null);
-                deleteButton.setVisibility(View.GONE);
-            }
-            else {
-                String username = super.getItem(position);
-                Drawable accountHead = mImageCache.get(username);
-                if (accountHead == null){
-                    accountHead = new BitmapDrawable(parent.getResources(), MinecraftAccount.getSkinFace(username));
-                    mImageCache.put(username, accountHead);
-                }
-                textview.setCompoundDrawables(accountHead, null, null, null);
-
-                deleteButton.setVisibility(View.VISIBLE);
-                deleteButton.setOnClickListener(v -> {
-                    showDeleteDialog(getContext(), position);
-                });
-            }
-            return convertView;
-        }
-
-
-
-        @NonNull
-        @Override
-        public View getView(int position, View convertView, @NonNull ViewGroup parent) {
-            View view = getDropDownView(position, convertView, parent);
-            view.findViewById(R.id.delete_account_button).setVisibility(View.GONE);
-            return view;
-        }
-
-        private void showDeleteDialog(Context context, int position) {
-            new AlertDialog.Builder(context)
-                    .setMessage(R.string.warning_remove_account)
-                    .setPositiveButton(android.R.string.cancel, null)
-                    .setNeutralButton(R.string.global_delete, (dialog, which) -> {
-                        onDetachedFromWindow();
-                        removeAccount(position);
-                    })
-                    .show();
+        @NonNull @Override
+        public View getView(int pos, View conv, @NonNull ViewGroup parent) {
+            View v = LayoutInflater.from(getContext()).inflate(R.layout.item_minecraft_account, parent, false);
+            ExtendedTextView tv = v.findViewById(R.id.account_item);
+            tv.setText(getItem(pos));
+            v.findViewById(R.id.delete_account_button).setVisibility(GONE);
+            return v;
         }
     }
-
-
-
 }
