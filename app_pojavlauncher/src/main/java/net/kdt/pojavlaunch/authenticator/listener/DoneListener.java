f94929ffd1b0dacbbcabe6aedfefdafb68069e36
DomainPixel <funpasscloud518@gmail.com>
2026-01-13 23:43:47 +0530

    Update DoneListener.java

--- app_pojavlauncher/src/main/java/net/kdt/pojavlaunch/authenticator/listener/DoneListener.java
+++ app_pojavlauncher/src/main/java/net/kdt/pojavlaunch/authenticator/listener/DoneListener.java
@@ -2,7 +2,17 @@ package net.kdt.pojavlaunch.authenticator.listener;
 
 import net.kdt.pojavlaunch.value.MinecraftAccount;
 
-/** Called when the login is done and the account received. guaranteed to be on the UI Thread */
+/** Called when the Quattro login is complete and the account is received. */
 public interface DoneListener {
     void onLoginDone(MinecraftAccount account);
+}
+
+/** Called when there is a complete failure during Quattro authentication. */
+public interface ErrorListener {
+    void onLoginError(Throwable errorMessage);
+}
+
+/** Called when a Quattro login step is started. */
+public interface ProgressListener {
+    void onLoginProgress(int step);
 }
