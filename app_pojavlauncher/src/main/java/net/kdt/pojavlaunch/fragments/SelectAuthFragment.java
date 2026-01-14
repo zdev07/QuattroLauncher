1d86755b471397e0eb433ffd9df68cb0efc00221
DomainPixel <funpasscloud518@gmail.com>
2026-01-14 10:06:17 +0530

    Update SelectAuthFragment.java

--- app_pojavlauncher/src/main/java/net/kdt/pojavlaunch/fragments/SelectAuthFragment.java
+++ app_pojavlauncher/src/main/java/net/kdt/pojavlaunch/fragments/SelectAuthFragment.java
@@ -3,27 +3,35 @@ package net.kdt.pojavlaunch.fragments;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
-
+import android.widget.TextView;
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
-
 import net.kdt.pojavlaunch.R;
 import net.kdt.pojavlaunch.Tools;
 
+/** Full Rewrite: Quattro Authentication Portal */
 public class SelectAuthFragment extends Fragment {
-    public static final String TAG = "AUTH_SELECT_FRAGMENT";
+    public static final String TAG = "QUATTRO_AUTH_FRAGMENT";
 
-    public SelectAuthFragment(){
+    public SelectAuthFragment() {
         super(R.layout.fragment_select_auth_method);
     }
 
     @Override
     public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        // Rebrand UI Text
+        TextView title = view.findViewById(R.id.auth_title);
+        if (title != null) title.setText("Welcome to Quattro");
+
         Button mMicrosoftButton = view.findViewById(R.id.button_microsoft_authentication);
         Button mLocalButton = view.findViewById(R.id.button_local_authentication);
 
-        mMicrosoftButton.setOnClickListener(v -> Tools.swapFragment(requireActivity(), MicrosoftLoginFragment.class, MicrosoftLoginFragment.TAG, null));
-        mLocalButton.setOnClickListener(v -> Tools.swapFragment(requireActivity(), LocalLoginFragment.class, LocalLoginFragment.TAG, null));
+        // Modernized Click Listeners
+        mMicrosoftButton.setOnClickListener(v -> 
+            Tools.swapFragment(requireActivity(), MicrosoftLoginFragment.class, "QUATTRO_MS_LOGIN", null));
+            
+        mLocalButton.setOnClickListener(v -> 
+            Tools.swapFragment(requireActivity(), LocalLoginFragment.class, "QUATTRO_LOCAL_LOGIN", null));
     }
 }
